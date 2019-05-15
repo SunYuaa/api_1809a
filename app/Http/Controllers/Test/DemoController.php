1cde20173d55910a58a54461a658ae49b3a05dce
SunYuaa <2861164631@qq.com>
2019-05-15 07:19:53 +0000

    bug

--- app/Http/Controllers/Test/DemoController.php
+++ app/Http/Controllers/Test/DemoController.php
@@ -20,9 +20,13 @@ class DemoController extends Controller
 //        header('Access-Control-Allow-Methods:OPTIONS,GET,POST');
 //        header('Access-Control-Allow-Headers:x-requested-with');
 
-        $name = $_POST['name'];
-        $pwd = $_POST['pwd'];
-        $email = $_POST['email'];
+//        $name = $_POST['name'];
+//        $pwd = $_POST['pwd'];
+//        $email = $_POST['email'];
+
+        $name = $request->input('name');
+        $pwd = $request->input('pwd');
+        $email = $request->input('email');
         $e = UserModel::where(['email'=>$email])->first();
         if($e){
             $response = [
