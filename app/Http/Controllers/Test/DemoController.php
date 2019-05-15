4e68641e457f6145a32d8730971186f394715361
SunYuaa <2861164631@qq.com>
2019-05-15 08:09:47 +0000

    bug

--- app/Http/Controllers/Test/DemoController.php
+++ app/Http/Controllers/Test/DemoController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Test;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Request;
 use App\Model\Exam\UserModel;
 
 class DemoController extends Controller
@@ -15,18 +14,16 @@ class DemoController extends Controller
     }
 
     //注册
-    public function reg(Request $request){
+    public function reg(){
 //        header('Access-Control-Allow-Origin: *');
 //        header('Access-Control-Allow-Methods:OPTIONS,GET,POST');
 //        header('Access-Control-Allow-Headers:x-requested-with');
 
-//        $name = $_POST['name'];
-//        $pwd = $_POST['pwd'];
-//        $email = $_POST['email'];
+        $name = $_POST['name'];
+        $pwd = $_POST['pwd'];
+        $email = $_POST['email'];
 
-        $name = $request->input('name');
-        $pwd = $request->input('pwd');
-        $email = $request->input('email');
+
         $e = UserModel::where(['email'=>$email])->first();
         if($e){
             $response = [
